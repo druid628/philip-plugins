2ba9f4a1dd3bcf01347b660d48f2a92c09124d7d
Micah Breedlove <druid628@gmail.com>
2012-10-23 12:42:27 -0500

    adds demolition man mode to swear jar

--- src/Philip/Plugin/SwearJarPlugin.php
+++ src/Philip/Plugin/SwearJarPlugin.php
@@ -18,23 +18,51 @@ class SwearJarPlugin extends BasePlugin
      */
     public function init()
     {
+
+        $swear_conf = array(
+          "normal" => array(
+              "cost" => 0.25,
+          ),
+          "demolition_man" => array(
+              "cost" => 1,
+          ),
+        );
+
+        $bot = $this->bot;
+        $config = $bot->getConfig();
+        $swear_mode = "normal";
+        if(isset($bot['swear_mode']) && $bot['swear_mode'] == "demolition_man")
+        {
+            $swear_mode = "demolition_man";
+        }
         $swears = array('fu+ck', 'sh+i+t', 'cu+nt', 'co+ck');
         $swear_jar = array();
 
         // Don't say bad words, kids.
         $re = '/' . implode('|', $swears) . '/i';
-        $this->bot->onChannel($re, function($event) use (&$swear_jar) {
-            $cost = 0.25;
+        $this->bot->onChannel($re, function($event) use (&$swear_jar, $swear_conf, $swear_mode) {
+            
             $who = $event->getRequest()->getSendingUser();
             if (!isset($swear_jar[$who])) {
                 $swear_jar[$who] = 0;
             }
 
-            $price = ($swear_jar[$who] += $cost);
+            $price = ($swear_jar[$who] += $swear_conf[$swear_mode]['cost']);
+            if( $swear_mode == "demolition_man" )
+            {
+                if($price == 1) {
+                    $responseOutput = sprintf("%s, you are fined %d credit for a violation of the Verbal Morality Statute.",  $who, $price);
+                } else {
+                    $responseOutput = sprintf("%s, you have been fined %d credit for a violation of the Verbal Morality Statute.",  $who, $price);
+                }
+            } else {
+                $responseOutput = sprintf("Mind your tongue $who! Now you owe \$%.2f to the swear jar.", $price);
+            }
+
             $event->addResponse(
                 Response::msg(
                     $event->getRequest()->getSource(),
-                    sprintf("Mind your tongue $who! Now you owe \$%.2f to the swear jar.", $price)
+                    $responseOutput
             ));
         });
     }
